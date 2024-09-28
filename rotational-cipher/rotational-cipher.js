bc2320ecfec7fabe827fb1574451b1bc8456d5eb
Alejandro <alejandromarr@gmail.com>
2024-09-28 02:18:15 +0200

    Replaced magic number with constant :technologist:

--- rotational-cipher/rotational-cipher.js
+++ rotational-cipher/rotational-cipher.js
@@ -1,9 +1,11 @@
+const ALPHABET_LENGTH = 26;
+
 export function rotate(plain, rotation) {
   const MIN_LOWERCASE = 'a'.charCodeAt(0);
   const MIN_UPPERCASE = 'A'.charCodeAt(0);
 
   return [...plain].map(char =>
-      /[A-Z]/.test(char) ? String.fromCharCode(MIN_UPPERCASE + (char.charCodeAt(0) - MIN_UPPERCASE + rotation) % 26) :
-          /[a-z]/.test(char) ? String.fromCharCode(MIN_LOWERCASE + (char.charCodeAt(0) - MIN_LOWERCASE + rotation) % 26) : char)
+      /[A-Z]/.test(char) ? String.fromCharCode(MIN_UPPERCASE + (char.charCodeAt(0) - MIN_UPPERCASE + rotation) % ALPHABET_LENGTH) :
+          /[a-z]/.test(char) ? String.fromCharCode(MIN_LOWERCASE + (char.charCodeAt(0) - MIN_LOWERCASE + rotation) % ALPHABET_LENGTH) : char)
       .join('');
 }
