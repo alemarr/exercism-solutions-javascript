21fbeb31b270a7f3c7bc2029fc86a52b50e5298a
Alejandro <alejandromarr@gmail.com>
2024-09-30 22:25:56 +0200

    Updated Complex Numbers solution

--- complex-numbers/complex-numbers.js
+++ complex-numbers/complex-numbers.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Complex Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class ComplexNumber {
   constructor(real, imaginary) {
     this._real = real;
